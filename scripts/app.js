279ec70cb80cd9e65114cf6f92e0c2e00da21a18
Jonas Karlsson <jonas.karlsson@valtech.se>
2015-05-22 10:54:37 +0200

    Moved some codes to separate files using directives to get more clean code. Detection if 500px near the end is working, functionality to load more content is however not working.

--- scripts/app.js
+++ scripts/app.js
@@ -7,32 +7,75 @@ app.filter('to_trusted', ['$sce', function($sce){
 }]);
 
 app.controller('FeedController', ['$http', '$scope', '$sce', function($http, $scope, $sce){
-	$scope.entries = {hej: "hej"};
+	$scope.entries = {};
 	this.button = "Load more";
 	$scope.currentSection = 0;
 	
-	$scope.getSection = function() {
-		console.log("hejjj");
-
+	$scope.nextSection = function() {
+		console.log("Get next section");
 
 		$http.get('test.json').
 		  success(function(data, status, headers, config) {
-		    // this callback will be called asynchronously
+		    // This callback will be called asynchronously
 		    // when the response is available
-		    $scope.entries = data.responseData.feed.entries.slice(0, ($scope.currentSection + 1) * 5);
+		    var sectionSize = 5;
+		    var currentIndex = $scope.currentSection * sectionSize;
+
+		    $scope.entries = data.responseData.feed.entries.slice(currentIndex, currentIndex + sectionSize);
 		    $scope.currentSection++;
 		    $scope.title = data.responseData.feed.title;
-		    
 		  });
 	};
 
-	 $scope.getSection();
+	$scope.nextSection();
 }]);
 
-app.directive('infiniteFeed', ['$http', function($http) {
+app.directive('infiniteFeed', function($window) {
 	return {
-		restrict: 'EA'
-	};
-}]);
+		restrict: 'EA',
+		templateUrl: 'feed.html',
+		link: function (scope, element, attributes, parentController) {
 
+			angular.element($window).bind('scroll', function() {
+				// console.log($window.pageYOffset + " ||||| " + getDocHeight() + " ||||| " + $window.innerHeight);
+
+				var endOffset = 500;
+
+				if ($window.pageYOffset >= (getDocHeight() - $window.innerHeight) - endOffset) {
+					console.log("Near end");
+				}	
+			});
+		}
+	};
+});
+
+app.directive('article', function() {
+	return {
+		restrict: 'EA',
+		templateUrl: 'article.html'
+	};
+});
+
+app.directive('navbar', function() {
+	return {
+		restrict: 'EA',
+		templateUrl: 'navbar.html',
+		controller: function($http, $scope){
+			$http.get('test.json').
+				success(function(data, status, headers, config) {
+					// This callback will be called asynchronously
+					// when the response is available
+					$scope.title = data.responseData.feed.title;
+				});
+		}
+	};
+});
+
+function getDocHeight() {
+    return Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
+}
 
