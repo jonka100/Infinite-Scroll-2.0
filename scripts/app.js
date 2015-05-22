f233c1d122639e84dabc6195d9d7bf9d6294c348
Jonas Karlsson <jonas.karlsson@valtech.se>
2015-05-22 16:12:38 +0200

    Tried to reduce memory usage, without success. Tried to hide elements that is not in the view, and detecting when scrolling up to view them again. But that didn't helt much.

--- scripts/app.js
+++ scripts/app.js
@@ -15,6 +15,10 @@ app.controller('FeedController', ['$http', '$scope', '$sce', '$window', function
 	$scope.entries = [];
 	this.button = "Load more";
 	$scope.currentSection = 0;
+	$scope.currentScrollSection = 0;
+	$scope.previousPageYOffset = 0;
+	$scope.pixelsScrolledUp = 0;
+	$scope.sectionSize = 5;
 	
 	$scope.nextSection = function() {
 		console.log("Get next section");
@@ -24,12 +28,24 @@ app.controller('FeedController', ['$http', '$scope', '$sce', '$window', function
 		  success(function(data, status, headers, config) {
 		    // This callback will be called asynchronously
 		    // when the response is available
-		    var sectionSize = 5;
-		    var currentIndex = $scope.currentSection * sectionSize;
+		    var currentIndex = $scope.currentSection * $scope.sectionSize ;
 
-		    if (data.responseData.feed.entries.length > currentIndex + sectionSize) {
-			    $scope.entries = $scope.entries.concat(data.responseData.feed.entries.slice(currentIndex, currentIndex + sectionSize));
+		    if (data.responseData.feed.entries.length > currentIndex + $scope.sectionSize ) {
+			    // $scope.entries = $scope.entries.concat(data.responseData.feed.entries.slice(currentIndex, currentIndex + sectionSize));
+		    	var tempEntries = data.responseData.feed.entries.slice(currentIndex, currentIndex + $scope.sectionSize );
+
+			   	for (var i = tempEntries.length - 1; i >= 0; i--) {
+			   		tempEntries[i].visible = true;
+			   	};
+
+			   	for (var i = $scope.entries.length - $scope.sectionSize ; i >= 0; i--) {
+			   		$scope.entries[i].visible = false;
+			   	};
+
+			   	$scope.entries = $scope.entries.concat(tempEntries);
+
 		    	$scope.currentSection++;
+		    	$scope.currentScrollSection = $scope.currentSection;
 		    	$scope.title = data.responseData.feed.title;
 		    	$scope.notLoading = true;
 		    }    
@@ -41,16 +57,33 @@ app.controller('FeedController', ['$http', '$scope', '$sce', '$window', function
 	// Detects if the user is scrolling
 	angular.element($window).bind('scroll', function() {
 		// console.log($window.pageYOffset + " ||||| " + getDocHeight() + " ||||| " + $window.innerHeight);
-
 		var endOffset = 500;
 
-		// Detects if the user is close to the end of the page, then load more content
-		if ($window.pageYOffset >= (getDocHeight() - $window.innerHeight) - endOffset) {
-			console.log("Near end");
-			if ($scope.notLoading) {
-				$scope.nextSection();
+		if ($window.pageYOffset > $scope.previousPageYOffset) {
+			// Detects if the user is close to the end of the page, then load more content
+			if ($window.pageYOffset >= (getDocHeight() - $window.innerHeight) - endOffset) {
+				console.log("Near end");
+				if ($scope.notLoading) {
+					$scope.nextSection();
+				}
 			}
-		}	
+		}
+		else {
+			console.log("Scrolling up!");
+			$scope.pixelsScrolledUp = $scope.pixelsScrolledUp + ($scope.previousPageYOffset - $window.pageYOffset);
+
+			if ($scope.pixelsScrolledUp >= 800) {
+				console.log("Load back unload content");
+				$scope.currentScrollSection--;
+
+				for (var i = $scope.sectionSize * $scope.currentScrollSection; i >= 0; i--) {
+			   		$scope.entries[i].visible = true;
+			   	};
+
+				$scope.pixelsScrolledUp = 0;
+			}
+		}
+		$scope.previousPageYOffset = $window.pageYOffset;
 	});
 }]);
 
